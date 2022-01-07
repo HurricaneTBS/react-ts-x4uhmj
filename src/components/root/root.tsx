804d10e43368a47b7dcf9eed4d9998f872cd851e
HurricaneTBS <634092350@qq.com>
2022-01-07 11:16:18 +0800

    1 file modified

--- src/components/root/root.tsx
+++ src/components/root/root.tsx
@@ -1,10 +1,17 @@
 import { Component } from 'react';
 import React, { Fragment } from 'react';
+import { Actions } from './index';
 type Props = {
   todos: Array<string>;
+  actions: Actions;
 };
 export default class Root extends Component<Props> {
+  componentDidMount(){
+    this.props.actions.addTodo("==---");
+  }
   render() {
+    console.log(this.props);
+    const { actions } = this.props;
     const todos = this.props.todos;
 
     const todoList =
